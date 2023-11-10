174930449ac904deb10d104a8dfd839e54aa2492
Ilja <iliagrigorev15@gmail.com>
2023-11-10 08:34:37 +0100

    Mofifying the start method and inserting it into the main method

--- Guesser.java
+++ Guesser.java
@@ -37,8 +37,8 @@ public class Guesser
    * rules() method, next the doGuesses() method.
    */
   public void start(){
-    // call the rules method here
-    // call the doGuesses() method here
+    rules(); // call the rules method here
+    doGuesses(); // call the doGuesses() method here
   }
 
   private void rules(){
--- GuessingGame.java
+++ GuessingGame.java
@@ -3,5 +3,7 @@ public class GuessingGame
     public static void main(String[] args) 
     {
         Guesser guesser = new Guesser(0, 1000);
+
+        guesser.start();
     }
 }
