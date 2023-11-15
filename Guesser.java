453c3d2b5cfd46721472ba93c2a3b1720f67f9b5
Ilja <iliagrigorev15@gmail.com>
2023-11-15 10:34:46 +0100

    Revert test

--- Guesser.java
+++ Guesser.java
@@ -1,5 +1,4 @@
 import java.io.Console;
-import java.util.Scanner;
 
 /**
  * Assignment 1 - Guessing Game
@@ -13,8 +12,6 @@ public class Guesser
 {
   private int low;
   private int high;
-
-  Scanner scn = new Scanner(System.in);
 
   /*
    * Task 1. Write code here for a constructor
@@ -62,25 +59,13 @@ public class Guesser
    * a valid reply. Return the String that you read from
    * the player.
    */
-  private String getReply()
-  {
+  private String getReply(){
+    String reply = null;
     // Write code here which reads a String from the console.
     // As long as it is not a valid reply (one of "T" and "F")
     // write an error message, and read a new reply.
     // When you have gotten a valid reply, return it.
-    while(true)
-    {
-      String reply = scn.nextLine();
-
-      if (reply.equals("T") || reply.equals("F"))
-      {
-        return reply;
-      }
-      else
-      {
-        System.out.println("This is not a valid input, please input T or F");
-      }
-    }
+    return reply;
   }
 
   private void doGuesses(){
